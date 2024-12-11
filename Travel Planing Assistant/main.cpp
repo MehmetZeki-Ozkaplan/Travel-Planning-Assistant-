983a75f4dd6806b1b8cfe0decec7c54300c58a48
Mehmet Zeki Özkaplan <123167485+MehmetZeki-Ozkaplan@users.noreply.github.com>
2024-12-11 19:25:18 +0300

    main.cpp

--- Travel Planing Assistant/main.cpp
+++ Travel Planing Assistant/main.cpp
@@ -1,19 +1,18 @@
-#include "library.h"		//Tanýmlanmýþ fonksiyonlarý içeren library kütüphanesi dahil edildi
+#include "library.h"							//Included library library with defined functions
 
-// Graf sýnýfýnýn kurucu-constructor fonksiyonu oluþturuldu deðer atama iþlemleri gerçekleþtirildi  
-// Bu fonksiyon, grafýn boyutunu ve komþuluk matrisini oluþturur
+// The constructor-constructor function of the graph class has been created, value assignment operations have been performed  
+// This function creates the size of the graph and the adjacency matrix
 Graf::Graf(int n) 
 {
-    this->n = n;  													 		// Grafýn düðüm sayýsýný alýr ve sýnýfýn n deðiþkenine atar
+    this->n = n;  							// Takes the number of nodes of the graph and assigns it to n variables of the class
 
-    // Komþuluk matrisi -1 ile doldurulur. -1, iki mekan arasýnda baðlantý olmadýðýný belirtmeyi saðlayacaktýr
-    for (int i = 0; i < n; i++)
-	{
+    // The adjacency matrix is filled with -1. -1 will indicate that there is no connection between the two venues
+    for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++) 
-		{
+	{
             komsulukMatrisi[i][j] = -1;
         }
-    }
+}
     mekanIsimleri[0] = "BASLANGIC (ABIDE KAVSAGI)";
     mekanIsimleri[1] = "BALIKLIGOL";
     mekanIsimleri[2] = "GOBEKLITEPE";
@@ -31,23 +30,23 @@ Graf::Graf(int n)
     mekanIsimleri[14] = "HZ. YAKUP KUYUSU";
     mekanIsimleri[15] = "GERMUS KILISESI";
     
-    // Mekan isimlerinin atamasý yapýldý
+    // Names of places were assigned
 }
 
-// grafOlustur() fonksiyonu ve gövde yapýsý oluþturuldu
-// grafOlustur() fonksiyonu mesafeler.txt dosyasýndan verileri okuyarak graf oluþturur
+// grafOlustur() function and body structure created
+// The grafOlustur() function creates a graph by reading data from a mesafeler.txt file
 void Graf::grafOlustur() 
 {
-    FILE* dosya = fopen("mesafeler.txt", "r"); 								// Dosya iþlemleri yapýlarak istenen dosya üzerinde okuma iþlemi yapýlýr
+    FILE* dosya = fopen("mesafeler.txt", "r"); 				// By performing file operations, reading is done on the desired file
     if (dosya == nullptr) 
-	{ 																		// Dosya açýlamazsa hata mesajý yazdýrýyoruz
+	{ 								// If the file cannot be opened, we print an error message
         cout << "Dosya acilamadi!" << endl;
         return;
-    }
+    	}
 
     int mekan1, mekan2, mesafe;
 
-    // Komþuluk matrisi sýfýrlandý - baþlangýçta tüm deðerleri 0 yapýldý - 
+    // The neighborhood matrix has been reset - initially all its values have been set to 0 - 
     for (int i = 0; i < n; i++) 
 	{
         for (int j = 0; j < n; j++) 
@@ -56,170 +55,171 @@ void Graf::grafOlustur()
         }
     }
 
-    // Dosyadaki veriler satýr satýr okutularak komþuluk matrisi doldurulur
+    // The data in the file is read line by line and the neighborhood matrix is filled in
     while (fscanf(dosya, "%d %d %d", &mekan1, &mekan2, &mesafe) == 3) 
 	{
-        // Geçerli ID'lerin belirlenen 0 - 15 aralýðýnda olup olmadýðý kontrol ediliyor
+        // Checking if valid IDs are within the specified range of 0 - 15
         if (mekan1 >= 0 && mekan1 < n && mekan2 >= 0 && mekan2 < n) 
-		{
+	{
             komsulukMatrisi[mekan1][mekan2] = mesafe;
-            komsulukMatrisi[mekan2][mekan1] = mesafe;						 // Yönlü deðilse karþýlýklý baðlantýyý da ekliyoruz
+            komsulukMatrisi[mekan2][mekan1] = mesafe;			 // If it is not directional, we also add the mutual link
         } 
 		else 
 		{
-            cout << "Hata: Dosyada geçersiz mekan numarasý: " << mekan1 << ", " << mekan2 << endl;
-        }
-    }
+            cout << "Hata: Dosyada geÃ§ersiz mekan numarasÄ±: " << mekan1 << ", " << mekan2 << endl;
+        	}
+ 	}
 
-    fclose(dosya); // Okunan dosya üzerinde kapatma iþlemi gerçekleþtirilir 
+    fclose(dosya); 							// A shutdown operation is performed on the read file 
 }
 
-// En kýsa yolun hesaplanmasý için Dijkstra algoritmasý kuullanýlýr 
+// En kÄ±sa yolun hesaplanmasÄ± iÃ§in Dijkstra algoritmasÄ± kuullanÄ±lÄ±r 
 void Graf::enKisaYolHesapla(int baslangic, int gezilecekMekanlar[], int secimSayisi)
 {
-    int mesafe[n];  														// Her bir mekana ait mesafeleri saklamak için mesafe dizisi
-    bool ziyaretEdildi[n]; 												    // Mekanlarýn ziyaret edilip edilmediðini kontrol etmek için kullanýlacak olan dizi
-    int oncelik[n];  														// Her bir mekanýn önceliðini tutan dizi
-    int toplamSure = 0, toplamMasraf = 0; 									// Gezi süresi ve masrafýný hesaplamak için deðiþkenler ve dedðiþken atamalarý yapýldý
+    int mesafe[n];  							// Distance array to store distances for each venue
+    bool ziyaretEdildi[n]; 						// Array to be used to check if venues have been visited
+    int oncelik[n];  							// The sequence that prioritizes each venue
+    int toplamSure = 0, toplamMasraf = 0; 				// Variables and variables were assigned to calculate the duration and expense of the trip
 
-    // Baþlangýçta tüm mesafeler çok büyük bir deðere (INT_MAX) ayarlanýr
-    // Ziyaret edilmedikleri için 'false' olarak ayarlanacaktor
-    // Öncelik dizisini -1 yapýlýr
+    // Initially, all distances are set to a very large value (INT_MAX)
+    // They will be set to 'false' because they are not visited
+    // The priority array is set to -1
     for (int i = 0; i < n; i++) 
 	{
         mesafe[i] = INT_MAX;
         ziyaretEdildi[i] = false;
         oncelik[i] = -1;
-    }
+    	}
 
-    mesafe[baslangic] = 0;  												// Baþlangýç noktasýnýn mesafesi sýfýr olarak atanýr
+    mesafe[baslangic] = 0;  						// The distance of the starting point is assigned as zero
 
-    // Dijkstra algoritmasýnýn temel döngüsü oluþturuldu
+    // The basic loop of the Dijkstra algorithm has been created
     for (int i = 0; i < n - 1; i++) 
 	{
         int minMesafe = INT_MAX, minIndex;
 
-        // En kýsa mesafeye sahip, henüz ziyaret edilmemiþ olan düðümü bulunur
-        for (int j = 0; j < n; j++) {
+        // The node with the shortest distance, which has not yet been visited, is found
+        for (int j = 0; j < n; j++) 
+	{
             if (!ziyaretEdildi[j] && mesafe[j] <= minMesafe) 
-			{
+		{
                 minMesafe = mesafe[j];
                 minIndex = j;
-            }
+            	}
         }
 
-        ziyaretEdildi[minIndex] = true; 									// Bu düðümü ziyaret edildi
+        ziyaretEdildi[minIndex] = true; 				// This node was visited
 
-        // Komþu düðümleri güncellenir
+        // Neighbor nodes are updated
         for (int k = 0; k < n; k++) 
-		{
+	{
             if (!ziyaretEdildi[k] && komsulukMatrisi[minIndex][k] != -1 &&
                 mesafe[minIndex] + komsulukMatrisi[minIndex][k] < mesafe[k]) 
-			{
+		{
                 mesafe[k] = mesafe[minIndex] + komsulukMatrisi[minIndex][k];
-                oncelik[k] = minIndex;  									// Öncelik güncellenir
-            }
+                oncelik[k] = minIndex;  				// Priority is updated
+            	}
         }
     }
 
-    // En kýsa yolun çýktýsýný ekrana yazdýrýlýr
+    // The output of the shortest path is printed on the screen
     cout << "\n\n <<--------\t Onerilen Gezi Rotasi \t-------->>" << endl;
     for (int i = 0; i < secimSayisi; i++) 
 	{
         cout << " --> "<< mekanIsimleri[gezilecekMekanlar[i]] ;
-    }
-	// Gezilecek mekanlarýn detaylarýný ekrana yazdýrýlýr
+    	}
+    // The details of the places to visit are printed on the screen
     cout << "\n\n <<--------\t Mekan Detaylari \t-------->>" << endl;
     for (int i = 0; i < secimSayisi; i++) 
 	{
         int mekan = gezilecekMekanlar[i];
-        // Gezi süresi ve masraf bilgilerini içeren mekan verileri
+        // Venue data including trip duration and expense information
         int geziSuresi, girisUcreti;
-        // Burada her mekanýn süresi ve giriþ ücreti tanýmlanýr
+        // Here, the duration of each venue and the entrance fee are defined
         if (mekan == 1) { geziSuresi = 30; girisUcreti = 10; }
         else if (mekan == 2) { geziSuresi = 120; girisUcreti = 90; }
         else if (mekan == 3) { geziSuresi = 90; girisUcreti = 150; }
         else if (mekan == 4) { geziSuresi = 50; girisUcreti = 50; }
         else if (mekan == 5) { geziSuresi = 45; girisUcreti = 100; }
         else if (mekan == 6) { geziSuresi = 30; girisUcreti = 8; }
-        // Ek bilgiler eklenebilir
+        // Additional information may be added
         toplamSure += geziSuresi;
         toplamMasraf += girisUcreti;
 
         cout << mekanIsimleri[mekan] << " ---> " << geziSuresi << " dakika, " << girisUcreti << " TL\n";
-    }
+    	}
 
-    // Toplam süre ve masraf ekrana yazdýrýlýr
+    // The total time and expense are printed on the screen
     cout << "\nToplam Gezi Suresi: " << toplamSure << " dk" << endl;
     cout << "Toplam Gezi Masrafi: " << toplamMasraf << " TL\n";
 }
 
-// dosyaOku() fonksiyonu ile mekanlar.txt dosyasýnýn içeriði okunur
+// dosyaOku() The contents of the mekanlar.txt file are read with the function
 void dosyaOku(Mekan mekanlar[], int &mekanSayisi) 
 {
     FILE* dosya = fopen("mekanlar.txt", "r");
 
     if (dosya == nullptr) 
-	{
-        cerr << "Dosya acilamadi!" << endl;									// cerr parametresi ile hata mesajý ile ekrana yazdýrýlýr
+    {
+        cerr << "Dosya acilamadi!" << endl;				 // It is printed on the screen with an error message with the CERR parameter
         return;
     }
 
     char satir[256];
-    // Dosyadaki her satýrý okunur
+    // Every line in the file is read
     while (fgets(satir, sizeof(satir), dosya)) 
 	{
         Mekan yeniMekan;
         char isimBuffer[100];
 
-        // Satýr iþlenip mekan bilgileri alýnýr
+        // The line is processed and the venue information is obtained
         if (sscanf(satir, "%d\t%[^\t]\t%d\t%d", &yeniMekan.id, isimBuffer, &yeniMekan.geziSuresi, &yeniMekan.girisUcreti) == 4) 
-		{
-            yeniMekan.isim = isimBuffer; 									// Mekan ismi string'e dönüþtürülür
-            mekanlar[mekanSayisi++] = yeniMekan; 							// Yeni mekanlar diziye eklenir
+	{
+            yeniMekan.isim = isimBuffer; 				 // The name of the place is converted to string
+            mekanlar[mekanSayisi++] = yeniMekan; 			 // New locations are added to the array
         }
     }
 
-    fclose(dosya); 															// Dosyayý kapatma iþlemi ile okunan mekanlar.txt dosyasý kapatýlýr
+    fclose(dosya); 							 // Closing the file closes the read mekanlar.txt file
 }
 
-// mekanSirala() fonksiyonu ile mekanlar.txt dosyasýnda belirtilen mekanlar giriþ ücretleri artan bir þekilde sýralanacaktýr
+// mekanSirala() With the function, the entrance fees of the venues specified in the mekanlar.txt file will be sorted in ascending order
 void mekanSirala() 
 {
-    Mekan mekanlar[100];													// Maksimum 100 mekan için dizi tanýmlanýyor
+    Mekan mekanlar[100];						 // A series is defined for a maximum of 100 venues
     int mekanSayisi = 0;
 
-    dosyaOku(mekanlar, mekanSayisi); 										// Dosyadaki mekanlar dosyaOku() fonksiyonu kullanýlarak okutulur
+    dosyaOku(mekanlar, mekanSayisi); 					 // The spaces in the file are read using the dosyaOku() function
 
     if (mekanSayisi == 0) 
 	{
         cout << "Mekanlar listesi bos!" << endl;
         return;
-    }
+    	}
 
-    // Mekanlar giriþ ücretine göre sýralanýr
+    // Venues are sorted by entrance fee
     for (int i = 0; i < mekanSayisi - 1; i++) 
 	{
         int minIndex = i;
         for (int j = i + 1; j < mekanSayisi; j++) {
             if (mekanlar[j].girisUcreti < mekanlar[minIndex].girisUcreti) 
-			{
+	    {
                 minIndex = j;
             }
         }
-        swap(mekanlar[i], mekanlar[minIndex]);  							// Dizi elemanlarýnýn yeri deðiþtirilir
+        swap(mekanlar[i], mekanlar[minIndex]);  			  // The location of the array elements is changed
     }
 
-    // Sýralanmýþ mekanlar ekrana yazdýrýlýr
+    // Sorted places are printed on the screen
     cout << "\nMekanlar (Giris Ucretine Gore Siralanmis):\n";
     for (int i = 0; i < mekanSayisi; i++) 
 	{
         cout << mekanlar[i].isim << " - " << mekanlar[i].girisUcreti << " TL" << endl;
-    }
+    	}
 }
 
 
-// menu() fonksiyonu ile mekan isimleri ekrana yazdýrýlacaktýr
+// menu() function, the names of the places will be printed on the screen
 void menu() 
 {
     cout << "<<--------\t Sanliurfa Gezi Rehberi \t-------->>" << endl;
@@ -244,36 +244,36 @@ void menu()
 
 int main() 
 {
-    // Gezi rehberinin menüsü yazdýrýlarak baþlanýr.
+    // It starts by printing the menu of the travel guide.
     menu();
 
-    // Graf nesnesi oluþturulur. 16 mekan var, bu yüzden 16 boyutlu bir graf oluþturulur.
+    // The graph object is created. There are 16 spaces, so a 16-dimensional graph is created.
     Graf g(16);
 
-    // Grafý oluþturuyoruz (mekanlar arasý mesafeler ve baðlantýlar burada yapýlýr).
+    // We create the graph (distances and connections between spaces are made here).
     g.grafOlustur();
 
-    // Kullanýcýdan gezmek istediði mekanlarýn ID'lerini alýnýr.
+    // The IDs of the places they want to visit are taken from the user.
     cout << "Gezmek Istediginiz mekanlari seciniz >> (Girilen ID numaralari arasinda bosluk birakiniz !): ";
     int gezilecekMekanlar[16], secimSayisi = 0;
 
-    // Kullanýcýdan mekan ID'leri alýnýr. Enter tuþuna basýldýðýnda veya 16 mekan girildiðinde döngü sonlanýr.
+    // Venue IDs are obtained from the user. The cycle ends when the Enter key is pressed or 16 venues are entered.
     while (cin >> gezilecekMekanlar[secimSayisi]) 
 	{
-        secimSayisi++; 														// Bir sonraki mekanýn ID'sine geçiyoruz.
+        secimSayisi++; 							       // We move on to the ID of the next venue.
         
-        // Kullanýcý bir satýr sonu (Enter) tuþuna basarsa döngü sonlanýr.
+        // If the user presses a line break (Enter) key, the loop ends.
         if (cin.get() == '\n') break;
-    }
+    	}
 
-    // Kullanýcýdan alýnan mekanlarýn fiyat listesi yazdýrýlýr.
+    // The price list of the venues received from the user is printed.
     cout << "\n\n <<--------\t Gezilecek Yerlerin Fiyat Listesi \t-------->>" << endl;
-    mekanSirala();  														// Fiyatlar sýralanarak yazdýrýlýr.
+    mekanSirala();  								// Prices are printed in order.
 
-    // Baþlangýç noktasý olarak "Baslangic (Abide Kavþaðý)" seçiliyor.
+    // "Baslangic (Abide KavÅŸaÄŸÄ±)" is chosen as the starting point.
     int baslangic = 0;
 
-    // Kullanýcý tarafýndan seçilen gezilecek mekanlar üzerinden en kýsa yol hesaplanýr.
+    // The shortest route is calculated based on the attractions selected by the user.
     g.enKisaYolHesapla(baslangic, gezilecekMekanlar, secimSayisi);
 
     return 0;
